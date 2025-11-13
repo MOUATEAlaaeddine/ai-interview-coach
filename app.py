b91f4a415bcefe375341c308ee3a82540cd5bce4
Alaa Eddine Mouate <eddine.alaa.mouate@gmail.com>
2025-11-13 22:58:02 +0100

    modify app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 from streamlit_js_eval import streamlit_js_eval
 
 # Setting up the Streamlit page configuration
-st.set_page_config(page_title="StreamlitChatMessageHistory", page_icon="💬")
+st.set_page_config(page_title="Interview Chat", page_icon="💬")
 st.title("Chatbot")
 
 # Initialize session state variables
